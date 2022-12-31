8ed42921bc2bb7b86b1dbe597b807ffcf566a368
HomeUser <superbuuza@gmail.com>
2022-12-31 16:53:36 +0800

    Create PracticalWork1.4

---
+++ PracticalWork1/PracticalWork1.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PracticalWork1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это первая часть задания: ");
+            Console.WriteLine("Hello World!!!");
+            Console.WriteLine();
+            Console.WriteLine("Это вторая часть задания: ");
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadKey();
+        }
+    }
+}
