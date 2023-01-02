05abea65254642372003dc2a70eef085b3b4bc5c
HomeUser <superbuuza@gmail.com>
2023-01-02 10:14:52 +0800

    allocated the second part of the project

---
+++ PracticalWork1/PracticalWork1.4.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PracticalWork1._4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это вторая часть задания: ");
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
--- PracticalWork1/PracticalWork1.4/Program.cs
+++ PracticalWork1/PracticalWork1.4/Program.cs
@@ -8,11 +8,6 @@ namespace PracticalWork1._4
         {
             Console.WriteLine("Это первая часть задания: ");
             Console.WriteLine("Hello World!!!");
-            Console.WriteLine();
-            Console.WriteLine("Это вторая часть задания: ");
-            Console.Write("Hello ");
-            Console.Write("World");
-            Console.Write("!!!");
             Console.ReadKey();
         }
     }
