38c654a8c8fd96f5f5f776dd2d24952837472383
HomeUser <superbuuza@gmail.com>
2023-01-15 22:43:41 +0800

    created PracticalWork4

---
+++ PracticalWork4/PracticalWork4_2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Матрица случайных чисел.");
+            while (true)
+            {
+                Console.Write("Введите количество строк: ");
+                int.TryParse(Console.ReadLine(), out int numberOfLines);
+                Console.Write("Введите количество столбцов: ");
+                int.TryParse(Console.ReadLine(), out int numberOfColumns);
+                if (numberOfLines != 0 && numberOfColumns != 0)
+                {
+                    Console.WriteLine($"Генерирую Матрицу А {numberOfLines} на {numberOfColumns}:\n");
+                    int[,] matrixA = new int[numberOfLines, numberOfColumns];
+                    Console.WriteLine($"Генерирую Матрицу B {numberOfLines} на {numberOfColumns}:\n");
+                    int[,] matrixB = new int[numberOfLines, numberOfColumns];
+                    int[,] matrixC = new int[numberOfLines, numberOfColumns];
+                    Random randomNumber = new Random();
+                    for (int i = 0; i < numberOfLines; i++)
+                    {
+                        for (int j = 0; j < numberOfColumns; j++)
+                        {
+                            matrixA[i, j] = randomNumber.Next(-9, 10);
+                            matrixB[i, j] = randomNumber.Next(-9, 10);
+                            matrixC[i, j] = matrixA[i, j] + matrixB[i, j];
+                        }
+                    }
+                    int index = 0;
+                    Console.WriteLine("Матрицы готовы. Вывожу:");
+                    Console.WriteLine("Матрица А:");
+                    foreach (int e in matrixA)
+                    {
+                        Console.Write($"{e,3} ");
+                        index++;
+                        if (index % numberOfColumns == 0)
+                        {
+                            Console.WriteLine();
+                        }
+                    }
+                    Console.WriteLine("Матрица B:");
+                    foreach (int e in matrixB)
+                    {
+                        Console.Write($"{e,3} ");
+                        index++;
+                        if (index % numberOfColumns == 0)
+                        {
+                            Console.WriteLine();
+                        }
+                    }
+                    Console.WriteLine("Матрица C (сумма матриц А и В):");
+                    foreach (int e in matrixC)
+                    {
+                        Console.Write($"{e,3} ");
+                        index++;
+                        if (index % numberOfColumns == 0)
+                        {
+                            Console.WriteLine();
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка. Вы ввели неправильно одно из двух значений. Пожалуйста, повторите ввод данных.\n");
+                    continue;
+                }
+
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PracticalWork4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Матрица случайных чисел.");
+            while (true)
+            {
+                Console.Write("Введите количество строк: ");
+                int.TryParse(Console.ReadLine(), out int numberOfLines);
+                Console.Write("Введите количество столбцов: ");
+                int.TryParse(Console.ReadLine(), out int numberOfColumns);
+                if (numberOfLines != 0 && numberOfColumns != 0)
+                {
+                    Console.WriteLine($"Генерирую массив {numberOfLines} на {numberOfColumns}:\n");
+                    int[,] arrayOfNumbers = new int[numberOfLines, numberOfColumns];
+                    Random randomNumber = new Random();
+                    int summAllNumbers = 0;
+                    for (int i = 0; i < numberOfLines; i++)
+                    {
+                        for (int j = 0; j < numberOfColumns; j++)
+                        {
+                            arrayOfNumbers[i, j] = randomNumber.Next(-9, 10);
+                            summAllNumbers += arrayOfNumbers[i, j];
+                            Console.Write($" {arrayOfNumbers[i, j],2} ");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.Write($"\nСумма всех элементов двумерного массива размерностью {numberOfLines} на {numberOfColumns}\n" +
+                        $"равна: {summAllNumbers}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка. Вы ввели неправильно одно из двух значений. Пожалуйста, повторите ввод данных.\n");
+                    continue;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
