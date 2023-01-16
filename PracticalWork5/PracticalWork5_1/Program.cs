3e5a438c7c2766b718bff2de755291b69b6a551b
HomeUser <superbuuza@gmail.com>
2023-01-16 22:19:03 +0800

    created PracticalWork5

---
+++ PracticalWork5/PracticalWork5_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            PrintWords(SplitText(Console.ReadLine()));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Метод получает строку с пробелами от пользователя, 
+        /// делит ее по пробелам на отдельные слова, формируя массив строк string[] outputWords
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] SplitText(string text)
+        {
+            string[] splitWords = text.Split(' ');
+            return splitWords;
+        }
+
+        /// <summary>
+        /// Метод получает от Метода SplitText массив строк string[] outputWords, перебирая его,
+        /// выводит каждый его элемент "word" в отдельной строке на экран
+        /// </summary>
+        /// <param name="splitWords"></param>
+        static void PrintWords(string[] splitWords)
+        {
+            foreach (var word in splitWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ PracticalWork5/PracticalWork5_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            Console.WriteLine(ReverseWords(Console.ReadLine()));
+            Console.ReadKey();
+        }
+
+        static string[] SplitText(string text)
+        {
+            string[] splitWords = text.Split(' ');
+            return splitWords;
+        }
+
+        static string ReverseWords(string inputPhrase)
+        {
+            string[] splitPhrase = SplitText(inputPhrase);
+            for (int i = 0, j = splitPhrase.Length -1; i < j; i++, j--)
+            {
+                string temp = splitPhrase[i];
+                splitPhrase[i] = splitPhrase[j];
+                splitPhrase[j] = temp;
+            }
+            string resultReverse = string.Empty;
+            foreach (var item in splitPhrase)
+            {
+                resultReverse = resultReverse + (item + " ");
+            }
+            return resultReverse;
+        }
+    }
+}
