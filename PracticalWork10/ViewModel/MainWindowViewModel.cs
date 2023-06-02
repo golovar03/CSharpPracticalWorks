99900786acfd029827eb5cd8b68462bd3460d87d
golovar <superbuuza@gmail.com>
2023-06-02 18:13:38 +0800

    Add first design

---
+++ PracticalWork10/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PracticalWork10.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork10.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string _title = "Главное окно";
+        /// <summary> Задание и вывод заголовка главного окна </summary>
+        public string Title 
+        { 
+            get => _title;
+            set=> Set(ref _title, value);
+        }
+    }
+}
