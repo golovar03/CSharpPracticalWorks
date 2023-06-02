69ac3b653e62ea0f29b3497c258991481e224967
golovar <superbuuza@gmail.com>
2023-06-02 15:19:08 +0800

    Create ViewModel Basicclass

---
+++ PracticalWork10/ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork10.ViewModel.Base
+{
+    internal class ViewModelBase
+    {
+    }
+}
